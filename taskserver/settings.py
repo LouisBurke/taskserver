38040bff575f14c14d6a4dfa94f95ca05b8ee8a3
Louis Burke <louis.j.burke@gmail.com>
2016-01-03 21:11:27 +0000

    Server backend

--- taskserver/settings.py
+++ taskserver/settings.py
@@ -76,10 +76,14 @@ WSGI_APPLICATION = 'taskserver.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
+DATABASES = {    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': ,
+        'USER' : ,
+        'PASSWORD' : ,
+        'HOST' : ,
+        'PORT' : ,
     }
 }
 
